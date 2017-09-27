2e47cf964263c7ea19e32fb36274f5b112bb06e6
TheGreenYonder <n.dalessandro@gmx.de>
2017-09-27 13:34:05 +0200

    very ugly check for duplicates in row

--- solver.py
+++ solver.py
@@ -1,19 +1,19 @@
 #console based solver
-
-#two dimensional list
-
-def prnt():
+import random
+import secrets
+#function to print grid
+def prnt_grd(sudoku):
 
     for y in range(9):
         s = ""
 
         for x in range(9):
-            if x%3 == 0:
+            if x % 3 == 0:
                 s = s + " |"
 
             s = s + " " + str(sudoku[y][x])
 
-        if y%3 == 0:
+        if y % 3 == 0:
             print(" -------------------------")
 
         print(s + " |")
@@ -21,9 +21,57 @@ def prnt():
     print(" -------------------------")
 
 
+def get_inpt(sudoku):
+    for y in range(9):
+        for x in range(9):
+            sudoku[y][x] = "X"
+            prnt_grd(sudoku)
+            inpt = input("Enter number or nothing for X - exit for exit: ")
 
-sudoku = [[0 for x in range(9)] for y in range(9)]
+            if inpt == "":
+                inpt = " "
+
+            sudoku[y][x] = inpt
+
+
+def check(sudoku, cp_sudoku):
+    t1 = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+    x = 0
+    y = 0
+    while y < 9:
+        x = 0
+        while x < 9:
+            if sudoku[y][x] == " ":
+                t1[x] = str(random.randrange(1, 10))
+            else:
+                t1[x] = sudoku[y][x]
+
+            x = x + 1
+
+        if len(set(t1)) != len(t1):
+            y = y - 1
+
+            for a in range(9):
+                for b in range(9):
+                    sudoku[a][b] = cp_sudoku[a][b]
+        else:
+            for z in range(9):
+                sudoku[y][z] = t1[z]
+                cp_sudoku[y][z] = t1[z]
+
+        y = y + 1
+
+
+#two dimensional list
 #sudoku[y][x] coordinates
+sudoku1 = [[" " for x in range(9)] for y in range(9)]
+sudoku_cp = [[" " for x in range(9)] for y in range(9)]
+get_inpt(sudoku1)
 
 
-prnt()
+for y in range(9):
+    for x in range(9):
+        sudoku_cp[y][x] = sudoku1[y][x]
+
+check(sudoku1, sudoku_cp)
+prnt_grd(sudoku1)
