540d402992763ab4274a81aecfbd5c17479b696a
TheGreenYonder <n.dalessandro@gmx.de>
2017-09-27 17:04:32 +0200

    some changes, not much better

--- solver.py
+++ solver.py
@@ -22,19 +22,27 @@ def prnt_grd(sudoku):
 
 
 def get_inpt(sudoku):
-    for y in range(9):
-        for x in range(9):
+    x = 0
+    y = 0
+    while y < 9:
+        x = 0
+        while x < 9:
+            print(str(x) + " " + str(y))
             sudoku[y][x] = "X"
             prnt_grd(sudoku)
-            inpt = input("Enter number or nothing/space for X: ")
+            inpt = input("Enter number or nothing/space: ")
 
             if inpt == "":
                 inpt = " "
 
             sudoku[y][x] = inpt
-
+            x = x + 1
+            if inpt == "x":
+                x = x - 2
+        y = y + 1
 
 def better(sudoku, cp_sudoku):
+    print("go")
     t1 = []
     t2 = []
     for y in range(9):
@@ -57,11 +65,12 @@ def better(sudoku, cp_sudoku):
         x = 0
         while x < 9:
             if t1[y][x] == " ":
-                t1[y][x] = str(random.randrange(1, 10))
+                t1[y][x] = str(random.randrange(0, 10))
 
-            x = x + 1
+            x = x + 11
 
-        if len(set(t1[x])) != len(t1[x]):
+        if len(set(t1[x])) != 9:
+            print("if")
             y = -1
 
             for a in range(18):
@@ -76,12 +85,18 @@ def better(sudoku, cp_sudoku):
 #sudoku[y][x] coordinates
 sudoku1 = [[" " for x in range(9)] for y in range(9)]
 sudoku_cp = [[" " for x in range(9)] for y in range(9)]
-get_inpt(sudoku1)
+#get_inpt(sudoku1)
 
 
 for y in range(9):
     for x in range(9):
         sudoku_cp[y][x] = sudoku1[y][x]
 
+for y in range(9):
+    for x in range(9):
+        sudoku1[y][x] = str((x + y + 1) % 10)
 
+sudoku1[5][5] = " "
+prnt_grd(sudoku1)
+print(sudoku1)
 better(sudoku1, sudoku_cp)
